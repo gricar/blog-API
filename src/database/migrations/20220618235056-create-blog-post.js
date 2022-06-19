93e4f8bdb0edae20e298ffd3340032501baf311d
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-06-18 21:01:45 -0300

    feat: criado model e migration do BlogPost

---
+++ src/database/migrations/20220618235056-create-blog-post.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'User',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blogPosts');
+  }
+};
---
+++ src/database/models/blogPost.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPosts', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  },
+  {
+    tableName: 'BlogPosts',
+  });
+
+  return BlogPost;
+};
