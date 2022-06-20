e69dae4c8ff3aeb926cabd8d1abc4308ab2433fe
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-06-20 15:50:21 -0300

    fix: name of attribute - postId

--- src/database/migrations/20220620174502-create-post-categories.js
+++ src/database/migrations/20220620174502-create-post-categories.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('PostCategories', {
-      postID: {
+      postId: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
---
+++ src/database/models/postCategory.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, foreignKey: true },
+    categoryId: { type: DataTypes.INTEGER, foreignKey: true }
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'PostCategories',
+  });
+
+  PostCategory.associate = ({ BlogPost, Category }) => {
+    BlogPost.belongsToMany(Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    Category.belongsToMany(BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
