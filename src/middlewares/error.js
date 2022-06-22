b50c34a6a1847d90c58a173f6f88e96768656819
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-06-22 18:51:28 -0300

    refactor: error midd

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,14 +1,11 @@
 module.exports = (err, _req, res, _next) => {
-  if (err.isJoi) {
-    return res.status(400)
-      .json({ message: err.details[0].message });
+  const { isJoi, statusCode, message } = err;
+
+  if (isJoi) {
+    return res.status(400).json({ message: err.details[0].message });
   }
 
-  if (err.statusCode) {
-    return res.status(err.statusCode).json({ message: err.message });
-  }
-
-  return res.status(500).json({
-    error: { message: `Internal server error: ${err.message}` },
-  });
+  return res
+    .status(statusCode || 500)
+    .json({ message } || { message: `Internal server error: ${message}` });
 };
