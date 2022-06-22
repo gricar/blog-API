f0d339d3d33bfe1b5216ef97c5631433d277cac5
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-06-22 10:23:57 -0300

    refactor: imported routes from index file

--- src/api.js
+++ src/api.js
@@ -1,18 +1,19 @@
 require('express-async-errors');
 const express = require('express');
 
+const { blogPosts, categories, login, user } = require('./routers');
 const { auth, error } = require('./middlewares');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/login', require('./routers/login'));
-app.use('/user', require('./routers/user'));
+app.use('/login', login);
+app.use('/user', user);
 
 app.use([auth]);
-app.use('/categories', require('./routers/categories'));
-app.use('/post', require('./routers/blogPosts'));
+app.use('/categories', categories);
+app.use('/post', blogPosts);
 
 app.all('*', (req, res) => res.status(404).json({ message: `Route '${req.path}' doesn't exist!` }));
 
---
+++ src/routers/index.js
@@ -0,0 +1,11 @@
+const blogPosts = require('./blogPosts');
+const categories = require('./categories');
+const login = require('./login');
+const user = require('./user');
+
+module.exports = {
+  blogPosts,
+  categories,
+  login,
+  user,
+};
