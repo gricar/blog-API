717e7c6cc6c8beeca934670aff4ef882c386ee38
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-06-25 14:27:09 -0300

    docs: swagger created basic doc created login path

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 require('express-async-errors');
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./utils/swaggerDocument.json');
 
 const { blogPosts, categories, login, user } = require('./routers');
 const { error } = require('./middlewares');
@@ -10,9 +12,9 @@ app.use(express.json());
 
 app.use('/login', login);
 app.use('/user', user);
-
 app.use('/categories', categories);
 app.use('/post', blogPosts);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // app.all('*', (req, res) => res.status(404).json({ message: `Route '${req.path}' doesn't exist!` }));
 
